cfa9f219545886b128f7af5c800c2f5aa41a052d
Liu Jiang <gerry@linux.alibaba.com>
2019-02-16 22:54:39 +0800

    Import volatile memory related crates from the crosvm project
    Import volatile memory related crates from the crosvm project, with minor
    modifications.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,9 @@ extern crate libc;
 use std::mem::size_of;
 use std::slice::{from_raw_parts, from_raw_parts_mut};
 
+pub mod volatile_memory;
+pub use volatile_memory::*;
+
 /// Types for which it is safe to initialize from raw data.
 ///
 /// A type `T` is `DataInit` if and only if it can be initialized by reading its contents from a
