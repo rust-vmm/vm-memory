f552a18375f4fdf88eab4fb1fe872597a1552285
Dr. David Alan Gilbert <dgilbert@redhat.com>
2019-03-22 17:26:23 +0000

    GuestMemoryMap: from_regions
    Add a method to take a vector of MmapRegion's that already exist and
    turn them into a GuestMemoryMap.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- src/mmap.rs
+++ src/mmap.rs
@@ -395,6 +395,29 @@ impl GuestMemoryMmap {
             regions: Arc::new(regions),
         })
     }
+
+    /// Creates a container and adds an existing set of mappings to it.
+    pub fn from_regions(ranges: Vec<GuestRegionMmap>) -> std::result::Result<Self, MmapError> {
+        if ranges.is_empty() {
+            return Err(MmapError::NoMemoryRegion);
+        }
+
+        for rangei in 1..ranges.len() {
+            let range = &ranges[rangei];
+            let last = &ranges[rangei - 1];
+            if last
+                .guest_base
+                .checked_add(last.mapping.len() as GuestUsize)
+                .map_or(true, |a| a > range.min_addr())
+            {
+                return Err(MmapError::MemoryRegionOverlap);
+            }
+        }
+
+        Ok(Self {
+            regions: Arc::new(ranges),
+        })
+    }
 }
 
 impl GuestMemory for GuestMemoryMmap {
@@ -505,6 +528,22 @@ mod tests {
         assert!(guest_mem.find_region(GuestAddress(0x600)).is_none());
         assert!(guest_mem.find_region(GuestAddress(0xa00)).is_some());
         assert!(guest_mem.find_region(GuestAddress(0xc00)).is_none());
+    }
+
+    #[test]
+    fn mapped_file_regions() {
+        let mut f = tempfile().unwrap();
+        let empty_buf = &[0; 16384];
+        assert!(f.write_all(empty_buf).is_ok());
+
+        let mem_map = MmapRegion::from_fd(&f, empty_buf.len(), 0).unwrap();
+        let guest_reg = GuestRegionMmap::new(mem_map, GuestAddress(0x8000));
+        let mut region_vec = Vec::new();
+        region_vec.push(guest_reg);
+        let guest_mem = GuestMemoryMmap::from_regions(region_vec).unwrap();
+        assert_eq!(guest_mem.num_regions(), 1);
+        assert!(guest_mem.find_region(GuestAddress(0)).is_none());
+        assert!(guest_mem.find_region(GuestAddress(0x8000)).is_some());
     }
 
     #[test]
