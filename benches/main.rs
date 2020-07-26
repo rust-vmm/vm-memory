e119007d7cd5166792b7dd3477058e15a051960b
Liu Jiang <gerry@linux.alibaba.com>
2020-07-26 23:23:40 +0800

    bench: add benchmark cases for Volatile interfaces
    Add benchmark cases for VolatileSlice/VolatileArrarRef interfaces.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- benches/main.rs
+++ benches/main.rs
@@ -5,21 +5,39 @@
 extern crate criterion;
 
 pub use criterion::{black_box, criterion_group, criterion_main, Criterion};
+#[cfg(feature = "backend-mmap")]
+use vm_memory::{GuestAddress, GuestMemoryMmap};
 
+mod guest_memory;
 mod mmap;
+mod volatile;
+
+use volatile::benchmark_for_volatile;
 
 #[cfg(feature = "backend-mmap")]
-use mmap::benchmark_for_mmap;
+pub fn create_guest_memory_mmap(size: u64, count: u64) -> GuestMemoryMmap {
+    let mut regions: Vec<(GuestAddress, usize)> = Vec::new();
+    for i in 0..count {
+        regions.push((GuestAddress(i * size), size as usize));
+    }
+
+    GuestMemoryMmap::from_ranges(regions.as_slice()).unwrap()
+}
 
 pub fn criterion_benchmark(_c: &mut Criterion) {
     #[cfg(feature = "backend-mmap")]
-    benchmark_for_mmap(_c);
+    mmap::benchmark_for_mmap(_c);
+}
+
+pub fn benchmark_guest_memory(_c: &mut Criterion) {
+    #[cfg(feature = "backend-mmap")]
+    guest_memory::benchmark_for_guest_memory(_c)
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(200).measurement_time(std::time::Duration::from_secs(50));
-    targets = criterion_benchmark
+    targets = criterion_benchmark, benchmark_guest_memory, benchmark_for_volatile
 }
 
 criterion_main! {
