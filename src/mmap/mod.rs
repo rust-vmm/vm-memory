506d7e249aea366a1ad58c1509203918736bc755
Hanna Czenczek <hreitz@redhat.com>
2025-05-13 12:01:41 +0200

    mmap: Wrap MmapRegion in Arc<>
    The vhost-user-backend crate will need to be able to modify all existing
    memory regions to use the VMM user address instead of the guest physical
    address once the IOMMU feature is switched on, and vice versa.  To do
    so, it needs to be able to modify regions’ base address.
    
    Because `GuestMemoryMmap` stores regions wrapped in an `Arc<_>`, we
    cannot mutate them after they have been put into the `GuestMemoryMmap`
    object; and `MmapRegion` itself is by its nature not clonable.  So to
    modify the regions’ base addresses, we need some way to create a new
    `GuestRegionMmap` referencing the same `MmapRegion` as another one, but
    with a different base address.
    
    We can do that by having `GuestRegionMmap` wrap its `MmapRegion` in an
    `Arc`, and adding a method to return that `Arc`, and a method to
    construct a `GuestRegionMmap` object from such a cloned `Arc.`
    
    Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

--- src/mmap/mod.rs
+++ src/mmap/mod.rs
@@ -15,6 +15,7 @@
 use std::borrow::Borrow;
 use std::ops::Deref;
 use std::result;
+use std::sync::Arc;
 
 use crate::address::Address;
 use crate::bitmap::{Bitmap, BS};
@@ -54,7 +55,7 @@ pub use windows::MmapRegion;
 /// in the virtual address space of the calling process.
 #[derive(Debug)]
 pub struct GuestRegionMmap<B = ()> {
-    mapping: MmapRegion<B>,
+    mapping: Arc<MmapRegion<B>>,
     guest_base: GuestAddress,
 }
 
@@ -62,7 +63,7 @@ impl<B> Deref for GuestRegionMmap<B> {
     type Target = MmapRegion<B>;
 
     fn deref(&self) -> &MmapRegion<B> {
-        &self.mapping
+        self.mapping.as_ref()
     }
 }
 
@@ -71,6 +72,11 @@ impl<B: Bitmap> GuestRegionMmap<B> {
     ///
     /// Returns `None` if `guest_base` + `mapping.len()` would overflow.
     pub fn new(mapping: MmapRegion<B>, guest_base: GuestAddress) -> Option<Self> {
+        Self::with_arc(Arc::new(mapping), guest_base)
+    }
+
+    /// Same as [`Self::new()`], but takes an `Arc`-wrapped `mapping`.
+    pub fn with_arc(mapping: Arc<MmapRegion<B>>, guest_base: GuestAddress) -> Option<Self> {
         guest_base
             .0
             .checked_add(mapping.size() as u64)
@@ -78,6 +84,16 @@ impl<B: Bitmap> GuestRegionMmap<B> {
                 mapping,
                 guest_base,
             })
+    }
+
+    /// Return a clone of the inner `Arc<MmapRegion>` (as opposed to [`.deref()`](Self::deref()),
+    /// which bypasses the `Arc`).
+    ///
+    /// The returned reference can be used to construct a new `GuestRegionMmap` with a different
+    /// base address (e.g. when switching between memory address spaces based on the guest physical
+    /// address vs. the VMM userspace virtual address).
+    pub fn get_mmap(&self) -> Arc<MmapRegion<B>> {
+        Arc::clone(&self.mapping)
     }
 }
 
