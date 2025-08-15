442297bc043ccc85e4e445d00547b2b8c4bfdf36
Hanna Czenczek <hreitz@redhat.com>
2025-08-15 16:21:49 +0200

    Implement GuestAddressSpace for IoMemory
    We want a trait like `GuestAddressSpace` for `IoMemory`, but just
    duplicating it into an `IoAddressSpace` trait is not so easy:
    
    We could rename the current `GuestAddressSpace` to `IoAddressSpace` and
    require `M: IoMemory` (instead of `M: GuestMemory`), and then define
    `GuestAddressSpace` as:
    
    ```rust
    pub trait GuestAddressSpace: IoAddressSpace<M: GuestMemory> {}
    
    impl<AS: IoAddressSpace> GuestAddressSpace for AS
    where
        AS::M: GuestMemory,
    {}
    ```
    
    But doing just this would break all existing `GuestAddressSpace` users,
    as they’d now need to import `IoAddressSpace` to use `memory()`.
    
    (Re-)Adding `GuestAddressSpace::memory()` as
    
    ```rust
    fn memory(&self) -> <Self as IoAddressSpace>::T {
        IoAddressSpace::memory(self)
    }
    ```
    
    also doesn’t (just) work, as it gets the compiler confused which
    `memory()` to use (between `GuestAddressSpace` and `IoAddressSpace`), so
    the `IoAddressSpace::memory()` method would need to be called
    differently.  However, I would find that a bit silly, and it would also
    then later require changes if the user wants to switch from
    `GuestMemory` to `IoMemory`.
    
    Instead just changing the `GuestAddressSpace::M: GuestMemory`
    requirement to `M: IoMemory` seems easier:
    - All callers that just use the `Bytes` interface remain completely
      unchanged,
    - It does break users that actually need the `GuestMemory` interface,
      but from what I have seen, that is only the case for
      `vhost::vhost_kern`.  There, we can simply require that
      `<AS as GuestAddressSpace>::M: GuestMemory`.
    
    Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

--- src/guest_memory.rs
+++ src/guest_memory.rs
@@ -222,7 +222,7 @@ impl FileOffset {
 /// ```
 pub trait GuestAddressSpace: Clone {
     /// The type that will be used to access guest memory.
-    type M: GuestMemory;
+    type M: IoMemory;
 
     /// A type that provides access to the memory.
     type T: Clone + Deref<Target = Self::M>;
@@ -233,7 +233,7 @@ pub trait GuestAddressSpace: Clone {
     fn memory(&self) -> Self::T;
 }
 
-impl<M: GuestMemory> GuestAddressSpace for &M {
+impl<M: IoMemory> GuestAddressSpace for &M {
     type M = M;
     type T = Self;
 
@@ -242,7 +242,7 @@ impl<M: GuestMemory> GuestAddressSpace for &M {
     }
 }
 
-impl<M: GuestMemory> GuestAddressSpace for Rc<M> {
+impl<M: IoMemory> GuestAddressSpace for Rc<M> {
     type M = M;
     type T = Self;
 
@@ -251,7 +251,7 @@ impl<M: GuestMemory> GuestAddressSpace for Rc<M> {
     }
 }
 
-impl<M: GuestMemory> GuestAddressSpace for Arc<M> {
+impl<M: IoMemory> GuestAddressSpace for Arc<M> {
     type M = M;
     type T = Self;
 
